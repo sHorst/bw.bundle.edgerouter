8025524934b6a4522b5a2fa84cea6fac82ff45db
Stefan Horst <stefan@ultrachaos.de>
2019-07-02 22:40:23 +0200

    fix for all possible hooks

--- hooks/add_edgeos.py
+++ hooks/add_edgeos.py
@@ -1,6 +1,15 @@
 
+def add_edgeos_to_node(node):
+    if 'edgeos' not in node.OS_FAMILY_LINUX:
+        node.OS_FAMILY_LINUX += ('edgeos', )
+        node.OS_FAMILY_UNIX += ('edgeos', )
+        node.OS_KNOWN += ('edgeos', )
 
 def node_apply_start(repo, node, interactive=False, **kwargs):
-    node.OS_FAMILY_LINUX += ('edgeos', )
-    node.OS_FAMILY_UNIX += ('edgeos', )
-    node.OS_KNOWN += ('edgeos', )
+    add_edgeos_to_node(node)
+
+def node_run_start(repo, node, command, **kwargs):
+    add_edgeos_to_node(node)
+
+def lock_add(repo, node, lock_id, items, expiry, comment, **kwargs):
+    add_edgeos_to_node(node)
