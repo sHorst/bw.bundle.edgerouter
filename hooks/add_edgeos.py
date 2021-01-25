9c71da33e2c520b514807637c720f704f280e6a4
Stefan Horst <stefan@ultrachaos.de>
2021-01-25 22:45:28 +0100

    cleanup code

--- hooks/add_edgeos.py
+++ hooks/add_edgeos.py
@@ -5,11 +5,14 @@ def add_edgeos_to_node(node):
         node.OS_FAMILY_UNIX += ('edgeos', )
         node.OS_KNOWN += ('edgeos', )
 
+
 def node_apply_start(repo, node, interactive=False, **kwargs):
     add_edgeos_to_node(node)
 
+
 def node_run_start(repo, node, command, **kwargs):
     add_edgeos_to_node(node)
+
 
 def lock_add(repo, node, lock_id, items, expiry, comment, **kwargs):
     add_edgeos_to_node(node)
